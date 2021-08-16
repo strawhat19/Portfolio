f2983bdd19804aa8d3b0e9db9f417a09257b100e
Rakib Ahmed <rakib987@gmail.com>
2021-08-15 21:46:48 -0400

    Spin Animation

--- assets/js/self/resume.js
+++ assets/js/self/resume.js
@@ -11,6 +11,38 @@ body.fadeIn(1000);
 var copyrightYear = $(`.copyright-year`);
 copyrightYear.html(moment().format(`YYYY`));
 
+// Intersection Observer
+// An API That Lets Us Detect When Elements Are In ViewPort
+let options2 = {
+    root: null,
+    rootMargin: `-250px 0px`,
+    threshhold: 0.05
+};
+
+// Declaring New Observer To Use for Observing All Elements with Class of .observe
+var observedItems = $(`.observe`);
+console.log(`Total Elements Being Observed: ${observedItems.length}`);
+
+let observer2 = new IntersectionObserver(inView, options2);
+observedItems.each((index,element) => {
+    observer2.observe(element);
+})
+
+// If The Element Is In The View Port
+function inView(entries) {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            $(entry.target).children().attr(`style,animation:none;`)
+            $(entry.target).fadeIn(5000);
+            $(entry.target).toggleClass(`inView`);
+            $(entry.target).removeClass(`notInView`);
+            $(entry.target).removeClass(`animationEnded`);
+        } else {
+            $(entry.target).toggleClass(`notInView`);
+        }
+    })
+}
+
 // Initializing First Map Screen
 function initmap(){
     var options = {
